589c06bf507652e812da74dae0753200b441b85c
FootsML <116684104+FootsML@users.noreply.github.com>
2023-07-19 14:26:37 -0500

    Update Get-ADDSDomainHealth.ps1

--- Scripts/AD DS/Get-ADDSDomainHealth.ps1
+++ Scripts/AD DS/Get-ADDSDomainHealth.ps1
@@ -1,27 +1,3 @@
-# Shows domain controller information of the current server
-Get-ADDomainController -Discover
-Read-UserInputToContinue
-
-# Active Directory services that need to be running.
-Get-Service adws,kdc,netlogon,dns
-Read-UserInputToContinue
-
-# Hidden shares that mest be available. SYSVOL and NETLOGON shares.
-Get-SMBshare
-Read-UserInputToContinue
-
-# Check replication between domain controllers.
-Repadmin /replsummary
-Read-UserInputToContinue
-
-# Analyze the state of the domain controllers in the forest.
-dcdiag
-Read-UserInputToContinue
-
-# Displays where the Flexible Single Master Operation roles are located.
-Netdom /query  FSMO
-Read-UserInputToContinue
-
 function Read-UserInputToContinue {
     if ($psISE) {
         Add-Type -AssemblyName System.Windows.Forms
@@ -31,3 +7,34 @@ function Read-UserInputToContinue {
         $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
     }
 }
+
+# Shows domain controller information of the current server
+Write-Host "Check domain controller information."
+Get-ADDomainController -Discover
+Read-UserInputToContinue
+
+# Active Directory services that need to be running.
+Write-Host "Check running Active Directory services."
+Get-Service adws,kdc,netlogon,dns
+Read-UserInputToContinue
+
+# Hidden shares that must be available. SYSVOL and NETLOGON shares.
+Write-Host "Check if the SYSVOL and NETLOGON SMB shares are available."
+Get-SMBshare
+Read-UserInputToContinue
+
+# Check replication between domain controllers.
+Write-Host "Check if replication between domain controllers is working."
+Repadmin /replsummary
+Read-UserInputToContinue
+
+# Analyze the state of the domain controllers in the forest.
+Write-Host "Analyze the state of the domain controller in the current forest."
+dcdiag
+Read-UserInputToContinue
+
+# Displays where the Flexible Single Master Operation roles are located.
+Write-Host "Check where the FSMO roles are located."
+Netdom /query FSMO
+Read-UserInputToContinue
+

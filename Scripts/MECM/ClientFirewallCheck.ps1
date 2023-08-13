756e3b54831f01c4628fb71a3bf14c728a1aa4cb
FootsML <116684104+FootsML@users.noreply.github.com>
2023-08-12 20:29:48 -0500

    Delete ClientFirewallCheck.ps1

--- Scripts/MECM/ClientFirewallCheck.ps1
+++
@@ -1,3 +0,0 @@
-$Port = 2701
-
-Get-NetFirewallPortFilter | Where-Object { $_.LocalPort -eq $Port } | Get-NetFirewallRule | Select-Object DisplayName,Direction,Action,Enabled | Format-Table -Wrap -AutoSize
